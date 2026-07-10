876473ab831fd4c60daa81a96786412966270000
CHIMIgb <156728324+CHIMIgb@users.noreply.github.com>
2026-07-09 23:05:29 -0700

    Fix env vars injection for Apache in Vercel Docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,5 +9,9 @@ if [ -n "$PORT" ]; then
     sed -i "s/80/$PORT/g" /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
 fi
 
+# Generar un archivo .env en tiempo de ejecución con las variables frescas de Vercel
+# Esto evita que Apache oculte las variables de entorno a PHP y sobreescribe cualquier .env viejo.
+printenv > /var/www/html/.env
+
 # Iniciar Apache
 apache2-foreground
